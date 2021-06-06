d10c5eff3fb15a9f37336480accce0734de9c768
Mahmoud Abdelsattar <jinkazama_m@yahoo.com>
2021-06-06 16:18:25 +0200

    Creating the movies list in the controller, model and view

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -1,2 +1,8 @@
 class MovieController < ApplicationController
+    MAX_RECORDS = 100
+
+    # GET method to get all products from database
+      def index
+        @movies = Movie.get_movies(MAX_RECORDS)
+      end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,14 @@
 class Movie < ApplicationRecord
-has_many: ratings
+    has_many :rating, foreign_key: "movie_id", class_name: "Rating"
+    RATING_COUNT_THRESHOLD = 20
+    Rating_ORDER = 20
+
+    def self.get_movies(max_records = 100)
+        Movie.select("title, genres, count(rating) as rating_count, round(avg(rating), 2) as average_rate")
+        .joins(:rating)
+        .group("movie_id")
+        .having("rating_count > ?", RATING_COUNT_THRESHOLD)
+        .order("average_rate DESC, rating_count DESC")
+        .limit(max_records)
+    end
 end
